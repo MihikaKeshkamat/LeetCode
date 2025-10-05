d6b728b457c04381b0782ca2c5a653b138212ea1
MihikaKeshkamat <168800544+MihikaKeshkamat@users.noreply.github.com>
2025-10-05 10:49:09 +0530

    Time: 1 ms (55.64%), Space: 41.7 MB (30.54%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char c : s.toCharArray()){
+            if(!stack.isEmpty() && stack.peekLast() == '(' && c == ')'){
+                stack.pollLast();
+            }else{
+                stack.offerLast(c);
+            }
+        }
+        return stack.size();
+    }
+}
