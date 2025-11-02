2f34b20d52eabb3cfbba6140d2742710f8e58645
MihikaKeshkamat <168800544+MihikaKeshkamat@users.noreply.github.com>
2025-11-02 17:32:50 +0530

    Time: 10 ms (93.12%), Space: 46.6 MB (10.92%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,15 +1,27 @@
 class Solution {
     public int numSquares(int n) {
-        int [] dp = new int[n+1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0; 
-        for(int i=1;i<=n;i++){
-            int min_val = Integer.MAX_VALUE;
-            for(int j=1;j*j<=i;j++){
-                min_val = Math.min(min_val, dp[i-j*j] + 1);
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[n+1];
+        q.offer(0);
+        vis[0] = true;
+        int level = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            level++;
+            for(int i=0;i<size;i++){
+                int curr = q.poll();
+                for(int j=1;j*j<=n;j++){
+                    int next = curr + j*j;
+                    if(next == n){
+                        return level;
+                    }
+                    if(next < n && !vis[next]){
+                        vis[next] = true;
+                        q.offer(next);
+                    }
+                }
             }
-            dp[i] = min_val;
         }
-        return dp[n];
+        return level;
     }
 }
